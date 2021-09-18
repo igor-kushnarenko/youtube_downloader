75caa6f1b8ea5091909b62c446218895c206914b
igor <anapa.igor@yandex.ru>
2021-09-18 18:56:07 +0300

    -получение ссылки вывел в отдельный класс;

--- app.py
+++ app.py
@@ -5,17 +5,13 @@ from pytube import YouTube
 
 
 class YouTubeDownloader:
-    re_url = re.compile(r'(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|playlist\?|'
-                        r'watch\?v=|watch\?.+(?:&|&#38;);v=))([a-zA-Z0-9\-_]{11})?(?:(?:\?|&|&#38;)'
-                        r'index=((?:\d){1,3}))?(?:(?:\?|&|&#38;)?list=([a-zA-Z\-_0-9]{34}))?(?:\S+)?')
 
-    def __init__(self):
-        self.url = None
+    def __init__(self, link):
+        self.url = link
         self.video_trigger = None
 
     def run(self):
-        url = self.get_url()
-        video = self.video_init(url)
+        video = self.video_init(self.url)
         self.user_choice(video)
 
     def user_choice(self, video):
@@ -32,6 +28,33 @@ class YouTubeDownloader:
             self.downloader(video)
             break
 
+    def audio_downloader(self, youtube_url):
+        file = youtube_url.streams.filter(only_audio=True).first()
+        file.download(output_path='files/audio')
+        os.rename(f'files/audio/{file.title}.mp4', f'files/audio/{file.title}.mp3')
+
+    def video_downloader(self, youtube_url):
+        file = youtube_url.streams.filter(progressive=True).desc().first()
+        file.download(output_path='files/videos')
+
+    def downloader(self, youtube_url):
+        if self.video_trigger:
+            self.video_downloader(youtube_url)
+        else:
+            self.audio_downloader(youtube_url)
+        print('Файл скачан успешно!')
+
+    def video_init(self, url):
+        youtube = YouTube(url=url)
+        print(youtube.title)
+        return youtube
+
+
+class GetLink:
+    re_url = re.compile(r'(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|playlist\?|'
+                        r'watch\?v=|watch\?.+(?:&|&#38;);v=))([a-zA-Z0-9\-_]{11})?(?:(?:\?|&|&#38;)'
+                        r'index=((?:\d){1,3}))?(?:(?:\?|&|&#38;)?list=([a-zA-Z\-_0-9]{34}))?(?:\S+)?')
+
     def get_url(self):
         while True:
             url_from_user = input('Вставьте ссылку: ')
@@ -43,23 +66,9 @@ class YouTubeDownloader:
                 print('Вставьте корректную ссылку!')
                 continue
 
-    def downloader(self, youtube_url):
-        if self.video_trigger:
-            file = youtube_url.streams.filter(progressive=True).desc().first()
-            file.download(output_path='files/videos')
-        else:
-            file = youtube_url.streams.filter(only_audio=True).first()
-            file.download(output_path='files/audio')
-            os.rename(f'files/audio/{file.title}.mp4', f'files/audio/{file.title}.mp3')
-        print('Файл скачан успешно!')
-
-    def video_init(self, url):
-        youtube = YouTube(url=url)
-        print(youtube.title)
-        return youtube
-
 
 if __name__ == '__main__':
     # 'https://www.youtube.com/watch?v=HglA72ogPCE'
-    download = YouTubeDownloader()
-    download.run()
+    link = GetLink().get_url()
+    downloader = YouTubeDownloader(link)
+    downloader.run()
--- youtube_console.py
+++ youtube_console.py
@@ -1,51 +1,32 @@
 import argparse
 
-from weather import Manager
+from app import YouTubeDownloader
 
-manager = Manager()
-
-parser = argparse.ArgumentParser(
-    prog='WeatherForecast',
-    description='Weather forecast',
-)
-subparser = parser.add_subparsers()
-
-"""Обновление базы"""
-parser_weather_date = subparser.add_parser(
-    'update',
-    help='Обновление данных о погоде. '
-         'При запросе происходит парсинг с сайта https://sinoptik.com.ru/. '
-         'Пример ввода: python weather_console.py update')
-parser_weather_date.set_defaults(func=manager.parse_data_send_to_db)
-
-"""Погода за сегодняшнее число"""
-parser_weather_date = subparser.add_parser(
-    'today',
-    help='Погода на сегодня. '
-         'Пример ввода команды для запуска: python weather_console.py today')
-parser_weather_date.set_defaults(func=manager.today_forecast)
-
-"""Погода на завтра"""
-parser_weather_date = subparser.add_parser(
-    'tomorrow',
-    help='Погода на завтра. '
-         'Пример ввода команды для запуска: python weather_console.py tomorrow')
-parser_weather_date.set_defaults(func=manager.tomorrow_forecast)
-
-"""Погода на определенную дату"""
-parser_weather_date = subparser.add_parser(
-    'to_date',
-    help='Погода на определенную дату. Что бы узнать погоду на 9 июля, '
-         'необходимо ввести номер месяца: 7 и дату: 9. '
-         'Пример ввода команды для запуска: python weather_console.py to_date')
-parser_weather_date.set_defaults(func=manager.get_card_from_date)
-
-"""Погода за период"""
-parser_weather_date = subparser.add_parser(
-    'from_to',
-    help='Погода за период с _ по _. '
-         'Пример ввода команды для запуска python weather_console.py from_to')
-parser_weather_date.set_defaults(func=manager.choice_dates_forecast)
-
+parser = argparse.ArgumentParser(description='Video downloader')
+parser.add_argument('link', type=str, help='Lin to videos')
 args = parser.parse_args()
-args.func()
+print(args.link)
+
+
+# downloader = YouTubeDownloader()
+#
+# parser = argparse.ArgumentParser(
+#     prog='Youtube Downloader',
+#     description='Youtube Downloader',
+# )
+# subparser = parser.add_subparsers()
+#
+# """Video download"""
+# video_downloader = subparser.add_parser(
+#     '-v',
+#     help='Video download. Command: python youtube_console.py <video_link> -v')
+# video_downloader.set_defaults(func=downloader.video_downloader)
+#
+# """Audio download"""
+# audio_downloader = subparser.add_parser(
+#     '-a',
+#     help='Video download. Command: python youtube_console.py <video_link> -a')
+# audio_downloader.set_defaults(func=downloader.audio_downloader)
+#
+# args = parser.parse_args()
+# args.func()
